7577bc449791cebc64f05baa19dca4a856a34423
Javonet <support@javonet.com>
2018-12-12 14:25:50 +0000

    init

---
+++ src/NBPExchangeRates/NBPExchangeRate.java
@@ -0,0 +1,105 @@
+package NBPExchangeRates;
+
+import Common.Activation;
+import static Common.Helper.Convert;
+import static Common.Helper.getGetObjectName;
+import static Common.Helper.getReturnObjectName;
+import static Common.Helper.ConvertToConcreteInterfaceImplementation;
+import Common.Helper;
+import com.javonet.Javonet;
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NObject;
+import com.javonet.api.NEnum;
+import com.javonet.api.keywords.NRef;
+import com.javonet.api.keywords.NOut;
+import com.javonet.api.NControlContainer;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Iterator;
+import java.lang.*;
+import NBPExchangeRates.*;
+
+public class NBPExchangeRate {
+  protected NObject javonetHandle;
+  /** GetProperty */
+  public java.lang.String getTableNumber() {
+    try {
+      java.lang.String res = javonetHandle.get("TableNumber");
+      if (res == null) return "";
+      return (java.lang.String) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return "";
+    }
+  }
+  /** GetProperty */
+
+  public java.math.BigDecimal getConversionRate() {
+    try {
+      java.math.BigDecimal res = javonetHandle.get("ConversionRate");
+      if (res == null) return null;
+      return (java.math.BigDecimal) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return null;
+    }
+  }
+  /** GetProperty */
+
+  public java.math.BigDecimal getExchangeRate() {
+    try {
+      java.math.BigDecimal res = javonetHandle.get("ExchangeRate");
+      if (res == null) return null;
+      return (java.math.BigDecimal) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return null;
+    }
+  }
+  /** GetProperty */
+
+  public java.lang.String getCurrency() {
+    try {
+      java.lang.String res = javonetHandle.get("Currency");
+      if (res == null) return "";
+      return (java.lang.String) res;
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+      return "";
+    }
+  }
+
+  public NBPExchangeRate(
+      java.lang.String tableNumber,
+      java.math.BigDecimal conversionRate,
+      java.math.BigDecimal exchangeRate,
+      java.lang.String currency) {
+    try {
+      javonetHandle =
+          Javonet.New(
+              "NBPExchangeRates.NBPExchangeRate",
+              tableNumber,
+              conversionRate,
+              exchangeRate,
+              currency);
+    } catch (JavonetException _javonetException) {
+      _javonetException.printStackTrace();
+    }
+  }
+
+  public NBPExchangeRate(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  public void setJavonetHandle(NObject handle) {
+    this.javonetHandle = handle;
+  }
+
+  static {
+    try {
+      Activation.initializeJavonet();
+    } catch (java.lang.Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
